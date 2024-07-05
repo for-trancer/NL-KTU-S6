b216e575156f7f7af32361b9bf52eb91daaa21c8
Arjun Ashok <125722020+for-trancer@users.noreply.github.com>
2024-07-05 21:33:18 +0530

    removed memset and added cliaddr

--- tcp_bi_client.c
+++ tcp_bi_client.c
@@ -1,22 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/socket.h>
 #include <netinet/in.h>
 #include <unistd.h>
 #include <string.h>
 
 #define MAX 1024
-#define PORT 9090
+#define PORT 8080
 
 void main()
 {
     	int sockfd;
-    	char buffer[MAX];
+    	char send_msg[MAX],receive_msg[MAX];
     	struct sockaddr_in servaddr;
 
     	sockfd = socket(AF_INET, SOCK_STREAM, 0);
-
-    	memset(&servaddr, 0, sizeof(servaddr));
 
     	servaddr.sin_family = AF_INET;
     	servaddr.sin_port = htons(PORT);
@@ -27,12 +24,11 @@ void main()
 	while(1)
 	{
 		printf("\nClient : ");
-		fgets(buffer,MAX,stdin);
-		send(sockfd,buffer,sizeof(buffer),0);
-		memset(buffer,MAX,0);
-		recv(sockfd,buffer,MAX,0);
-		printf("\nServer : %s",buffer);
-		if(strcmp(buffer,"bye\n")==0)
+		fgets(send_msg,MAX,stdin);
+		send(sockfd,send_msg,sizeof(send_msg),0);
+		recv(sockfd,receive_msg,MAX,0);
+		printf("\nServer : %s",receive_msg);
+		if((strcmp(send_msg,"bye\n")==0)||(strcmp(receive_msg,"bye\n")==0))
 		{
 			break;
 		}
