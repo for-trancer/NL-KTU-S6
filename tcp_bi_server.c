68cc39df88b5adcef8c61a6f12de7542aeef87bd
Arjun Ashok <125722020+for-trancer@users.noreply.github.com>
2024-07-05 21:32:20 +0530

    added cliaddr and removed memset

--- tcp_bi_server.c
+++ tcp_bi_server.c
@@ -1,23 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/socket.h>
 #include <netinet/in.h>
 #include <unistd.h>
 #include <string.h>
 
 #define MAX 1024
-#define PORT 9090
+#define PORT 8080
 
 void main()
 {
-	int sockfd, newsockfd;
+	int sockfd, newsockfd,len;
 	struct sockaddr_in servaddr, cliaddr;
-    	socklen_t len;
-    	char buffer[MAX];
+    	char send_msg[MAX],receive_msg[MAX];
 
     	sockfd = socket(AF_INET, SOCK_STREAM, 0);
-
-    	memset(&servaddr, 0, sizeof(servaddr));
 
     	servaddr.sin_family = AF_INET;
     	servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
@@ -33,13 +29,12 @@ void main()
 	
 	while(1)
 	{
-		memset(buffer,0,MAX);
-		recv(newsockfd,buffer,MAX,0);
-		printf("\nClient : %s\n",buffer);
+		recv(newsockfd,receive_msg,MAX,0);
+		printf("\nClient : %s\n",receive_msg);
 		printf("Server : ");
-		fgets(buffer,MAX,stdin);
-		send(newsockfd,buffer,strlen(buffer),0);
-		if(strcmp(buffer,"bye\n")==0)
+		fgets(send_msg,MAX,stdin);
+		send(newsockfd,send_msg,sizeof(send_msg),0);
+		if((strcmp(send_msg,"bye\n")==0)||(strcmp(receive_msg,"bye\n")==0))
 		{
 			break;
 		}
